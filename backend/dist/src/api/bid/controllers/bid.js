08cc23a1181d70b3156315b2c7b912edd56dfe3f
ozayo <ozay.ozdemir@gmail.com>
2024-11-25 10:49:43 +0100

    removed dist folder from git

--- backend/dist/src/api/bid/controllers/bid.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * bid controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::bid.bid');
