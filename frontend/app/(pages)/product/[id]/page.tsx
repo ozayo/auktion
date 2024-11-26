06a7d60af86701a340b02966c77cefd13da60fc3
ozayo <ozay.ozdemir@gmail.com>
2024-11-26 15:07:24 +0100

    added ProductImage component and fix some design

--- frontend/app/(pages)/product/[id]/page.tsx
+++ frontend/app/(pages)/product/[id]/page.tsx
@@ -1,18 +1,20 @@
 // app/product/[id]/page.tsx
-
 "use client";
 
-import { useState, useEffect } from 'react';
-import { useParams } from 'next/navigation';
-import { fetchAPI, API_URL } from '@/lib/api';
-import BidForm from '@/components/BidForm';
-import BidderList from '@/components/BidderList';
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import { fetchAPI, API_URL } from "@/lib/api";
+import BidForm from "@/components/BidForm";
+import BidderList from "@/components/BidderList";
+import ProductImage from "@/components/ProductImage";
 
 export default function ProductPage() {
   const params = useParams();
   const documentId = params?.id;
   const [product, setProduct] = useState<any>(null);
   const [sortedBids, setSortedBids] = useState<any[]>([]);
+  const highestBid = sortedBids.length > 0 ? sortedBids[0].Amount : null;
+
 
   const fetchProductData = async () => {
     if (!documentId) return;
@@ -35,7 +37,7 @@ export default function ProductPage() {
         setProduct(null);
       }
     } catch (error) {
-      console.error('Error retrieving product data:', error);
+      console.error("Error retrieving product data:", error);
       setProduct(null);
     }
   };
@@ -54,32 +56,48 @@ export default function ProductPage() {
     return <div>Product not found or loading...</div>;
   }
 
-  const { id: productId, title, description, price, main_picture, category_name, categories } = product;
+  const { title, description, price, main_picture, gallery, categories } =
+    product;
 
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">{title}</h1>
-      {/* Product Information*/}
-      <img
-        src={
-          main_picture?.url
-            ? `${API_URL}${main_picture.url}`
-            : '/placeholder.png'
-        }
-        alt={title}
-        className="w-full h-64 object-cover mb-4"
-      />
-      <p>{description}</p>
-      <p className="text-gray-700 mt-2">Utgångspris: {price} SEK</p>
-      <p className="text-gray-700 mt-2">
-        Categories: {categories.map((cat: any) => cat.category_name).join(', ') || 'No categories available'}
-      </p>
-
-      {/* List of Bidders*/}
-      <BidderList bids={sortedBids} />
-
-      {/* Bid Submission Form */}
-      <BidForm productId={productId} />
-    </main>
+    <div className="py-6 grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <div className="flex flex-col">
+        {/* Product Image */}
+        <ProductImage
+          mainPicture={{
+            url: `${API_URL}${main_picture.url}`
+          }}
+          gallery={gallery?.map((img: any) => ({
+            url: `${API_URL}${img.url}`
+          }))}
+        />
+      </div>
+      <div className="flex flex-col">
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <p className="text-gray-600 mt-2">
+          Categories:{" "}
+          {categories
+            ?.map((cat: any) => cat.category_name)
+            .join(", ") || "No categories available"}
+        </p>
+        <div className="flex items-center justify-between mt-4">
+          <p className="text-gray-600 ">Utgångspris: {price} SEK</p>
+          {highestBid !== null && (
+            <p className="text-gray-600 font-bold">Högsta bud: {highestBid} SEK</p>
+          )}
+          <p className="text-gray-600">12 bid</p>
+          <p className="text-gray-600">3 DAYS 12 HOURS 8 MIN</p>
+        </div>
+        {/* Bid Submission Form */}
+        <BidForm productId={product.id} />
+        {/* List of Bidders */}
+        <BidderList bids={sortedBids} />
+      </div>
+      <div className="lg:col-span-2 mt-8">
+        <h2 className="text-2xl font-bold my-2">Beskrivning</h2>
+        <p className="whitespace-pre-line">{description}</p>
+      </div>
+      
+    </div>
   );
 }
