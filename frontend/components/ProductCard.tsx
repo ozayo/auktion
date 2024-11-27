a7684ad93553a934b8dda6f2520ef8050a040272
ozayo <ozay.ozdemir@gmail.com>
2024-11-27 15:02:49 +0100

    added category on card

--- frontend/components/ProductCard.tsx
+++ frontend/components/ProductCard.tsx
@@ -12,7 +12,7 @@ export default function ProductCard({ product }: ProductCardProps) {
     return null; // eller visa ett lämpligt felmeddelande
   }
 
-  const { id, title, price, main_picture } = product;
+  const { id, title, price, main_picture, categories} = product;
 
   const imageUrl = main_picture?.url
     ? `${API_URL}${main_picture.url}`
@@ -23,6 +23,11 @@ export default function ProductCard({ product }: ProductCardProps) {
       <img src={imageUrl} alt={title} className="w-full h-48 object-cover mb-2" />
       <h2 className="text-xl font-semibold">{title}</h2>
       <p className="text-gray-700">Utgångspris: {price} SEK</p>
+      <p className="text-gray-600 mt-2">
+          Categories:{" "}
+          {categories?.map((cat: any) => cat.category_name).join(", ") ||
+            "No categories available"}
+        </p>
       <Link href={`/product/${product.documentId}`} className="text-blue-500 mt-2 inline-block">
         Se Detaljer
       </Link>
