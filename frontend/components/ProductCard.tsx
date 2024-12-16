1ae4fe2e26db2a7c553618af3d12ab5972fb4743
ozayo <ozay.ozdemir@gmail.com>
2024-12-16 15:26:58 +0100

    some fix

--- frontend/components/ProductCard.tsx
+++ frontend/components/ProductCard.tsx
@@ -9,7 +9,6 @@ interface ProductCardProps {
   userBid?: number | null; // Optional user's highest bid (for my_page)
   onFavoriteChange?: () => void;
   borderless?: boolean;
- 
 }
 
 export default function ProductCard({
@@ -18,7 +17,6 @@ export default function ProductCard({
   userBid = null, // Default to no user bid
   onFavoriteChange,
   borderless = false,
-  
 }: ProductCardProps) {
   if (!product) {
     console.error("Product is undefined:", product);
@@ -69,41 +67,46 @@ export default function ProductCard({
               className="w-full h-48 object-cover mb-2"
             />
             <div className="absolute top-0 left-0">
-              {categories?.length > 0 &&
+              {categories?.length > 0 ? (
                 categories.map((category: any, index: number) => (
                   <span
                     className="text-white rounded-full bg-blue-950 py-1 px-4 text-sm"
                     key={category.id || `category-${index}`}
                   >
                     {category.category_name}
-
-                  {index < categories.length - 1 && ", "}
-                </span>
-              ))
-            : "No categories available"}
-        </div>
-      </div>
-      <div className="product-details flex flex-col">
-        <div className="title min-h-16">
-          {/* Product Title */}
-          <h2 className="text-xl font-semibold">{title}</h2>
-        </div>
-        <div className="middle">
-          <div className="flex items-center justify-between py-2">
-            <div className="flex flex-col text-center">
-              {/* Base Price */}
-              <p className="text-gray-600 text-xs">Utgångspris</p>
-              <p className="text-gray-900 font-bold text-sm">{price ?? 0} SEK</p>
+                    {index < categories.length - 1 && ", "}
+                  </span>
+                ))
+              ) : (
+                <span>No categories available</span>
+              )}
             </div>
-            <div className="flex flex-col text-center">
-              {/* Highest Bid */}
-              {highestBid !== null && (
-                <>
-                  <p className="text-gray-600 text-xs">Ledande bud</p>
+          </div> {/* Kapanış etiketi eklendi */}
+          <div className="product-details flex flex-col">
+            <div className="title min-h-16">
+              {/* Product Title */}
+              <h2 className="text-xl font-semibold">{title}</h2>
+            </div>
+            <div className="middle">
+              <div className="flex items-center justify-between py-2">
+                <div className="flex flex-col text-center">
+                  {/* Base Price */}
+                  <p className="text-gray-600 text-xs">Utgångspris</p>
                   <p className="text-gray-900 font-bold text-sm">
-                    {highestBid ? `${highestBid} SEK` : "Inga bud"}
+                    {price ?? 0} SEK
                   </p>
                 </div>
+                <div className="flex flex-col text-center">
+                  {/* Highest Bid */}
+                  {highestBid !== null && (
+                    <>
+                      <p className="text-gray-600 text-xs">Ledande bud</p>
+                      <p className="text-gray-900 font-bold text-sm">
+                        {highestBid ? `${highestBid} SEK` : "Inga bud"}
+                      </p>
+                    </>
+                  )}
+                </div> 
                 {userBid !== null && (
                   <div className="flex flex-col text-center">
                     {/* My Highest Bid */}
@@ -122,25 +125,27 @@ export default function ProductCard({
                     </p>
                   </div>
                 )}
-              </div>
 
-              <div className="items-center flex flex-col justify-center min-h-14 mb-3">
-                {/* Remaining Time */}
-                {remainingTime ? (
-                  <>
-                    <p className="text-gray-600 text-xs">Budgivning avslutas</p>
-                    <p className="text-gray-800 font-bold text-lg">
-                      {remainingTime}
-                    </p>
-                  </>
-                ) : (
-                  <p className="text-red-500">Budgivningen avslutades</p>
-                )}
+                <div className="items-center flex flex-col justify-center min-h-14 mb-3">
+                  {/* Remaining Time */}
+                  {remainingTime ? (
+                    <>
+                      <p className="text-gray-600 text-xs">
+                        Budgivning avslutas
+                      </p>
+                      <p className="text-gray-800 font-bold text-lg">
+                        {remainingTime}
+                      </p>
+                    </>
+                  ) : (
+                    <p className="text-red-500">Budgivningen avslutades</p>
+                  )}
+                </div>
               </div>
-            </div>
-            {/* <Link className='flex items-center group font-bold text-black justify-end' href={`/product/${product.documentId}`}>
+              {/* <Link className='flex items-center group font-bold text-black justify-end' href={`/product/${product.documentId}`}>
           Lägg ett bud <FaLongArrowAltRight className='ml-2 group-hover:ml-2' />
         </Link> */}
+            </div>
           </div>
         </div>
       </Link>
